026c16eb7636052e43159e9d6d8e2779611b8d5c
Marina Cazzola <marina.cazzola@unimib.it>
2021-11-02 16:51:12 +0100

    [oefiff] better display of statement in parity (using wims_question)(wims_question defined in WIMS-4.17e) + embed

--- U1~logic~oefiff.en/src/cpp/parity.cpp
+++ U1~logic~oefiff.en/src/cpp/parity.cpp
@@ -12,8 +12,8 @@ target=parity
 \text{gnec=\gtype=1?\nec:\suf}
 \text{gsuf=\gtype=1?\suf:\nec}
 \text{choices=\iff,\nec,\suf,\non}
-\text{prod=<i>ab</i>}
-\text{sum=<i>a</i>+<i>b</i>}
+\text{prod=\(a b\)}
+\text{sum=\(a+b\)}
 \text{t1=\gtype=1?\prod:\sum}
 \text{t2=\gtype=1?\sum:\prod}
 \integer{p=random(1,2)}
@@ -22,6 +22,10 @@ target=parity
 \text{good=item(\p,\suf,\nec)}
 
 #include "lang.inc"
-\statement{\name_statement}
+
+\statement{\name_statement1
+<div class="wims_question">\t1 \name_is \name_cases_list[\p]</div>
+\name_statement2 \embed{c1}
+\name_statement3 \t2 \name_statement4 \name_cases_list[\p]}
 
 \choice{}{\good}{\choices}
