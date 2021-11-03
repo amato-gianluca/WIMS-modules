b9594397b0dd9628453f2658f8527afe48101213
Marina Cazzola HP <marina.cazzola@unimib.it>
2021-11-03 07:45:40 +0100

    [oefiff] typo

--- U1~logic~oefiff.en/src/cpp/parity.cpp
+++ U1~logic~oefiff.en/src/cpp/parity.cpp
@@ -17,8 +17,9 @@ target=parity
 \text{t1=\gtype=1?\prod:\sum}
 \text{t2=\gtype=1?\sum:\prod}
 \integer{p=random(1,2)}
-\text{parity=item(\p,\name_odd,\name_even)}
-\text{parity2=item(\p,\name_odd,\name_even)}
+\integer{q=\p=1?2:1}
+text{parity=item(\p,\name_odd,\name_even)}
+text{parity2=item(\p,\name_odd,\name_even)}
 \text{good=item(\p,\suf,\nec)}
 
 #include "lang.inc"
@@ -26,6 +27,6 @@ target=parity
 \statement{\name_statement1
 <div class="wims_question">\t1 \name_is \name_cases_list[\p]</div>
 \name_statement2 \embed{c1}
-\name_statement3 \t2 \name_statement4 \name_cases_list[\p]}
+\name_statement3 \t2 \name_statement4 \name_cases_list[\q]}
 
 \choice{}{\good}{\choices}
